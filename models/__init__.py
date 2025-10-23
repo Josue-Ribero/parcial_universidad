296ebb54f11857e31745420c3056dce0d2f04831
Josue-Ribero <josueribero95@gmail.com>
2025-10-23 17:57:36 -0500

    Init de los modelos

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+from .curso import Curso, CursoCreate, CursoUpdate, CursoDelete
+from .estudiante import Estudiante, EstudianteCreate, EstudianteUpdate, EstudianteDelete
+from .matricula import Matricula, MatriculaCreate, MatriculaUpdate, MatriculaDelete
+
+__all__ = [
+    "Curso", "CursoCreate", "CursoUpdate", "CursoDelete",
+    "Estudiante", "EstudianteCreate", "EstudianteUpdate", "EstudianteDelete",
+    "Matricula", "MatriculaCreate", "MatriculaUpdate", "MatriculaDelete",
+]
