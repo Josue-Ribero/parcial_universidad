39c7f308f64a51531ab8516f7dcf5d33ca1720e0
Josue-Ribero <josueribero95@gmail.com>
2025-10-23 16:29:23 -0500

    Enumeraciones iniciales

---
+++ utils/enum.py
@@ -0,0 +1,24 @@
+# Importacion de Enum
+from enum import Enum
+
+# Cantidad de creditos de un curso
+class CreditosCurso(Enum):
+    UNO = 1
+    DOS = 2
+    TRES = 3
+    CUATRO = 4
+
+# Semestre que esta cursando el estudiante
+class Semestre(Enum):
+    PRIMERO = 1
+    SEGUNDO = 2
+    TERCERO = 3
+    CUARTO = 4
+    QUINTO = 5
+    SEXTO = 6
+    SEPTIMO = 7
+    OCTAVO = 8
+    NOVENO = 9
+    DECIMO = 10
+    UNDECIMO = 11
+    DUODECIMO = 12
