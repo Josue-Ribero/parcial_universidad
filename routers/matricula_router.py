d2f1c2963e7fb529cd4504f56f99a9d12a58f808
Josue-Ribero <josueribero95@gmail.com>
2025-10-24 16:49:03 -0500

    Ruta para finalizar un curso por parte de un estudiante

--- routers/matricula_router.py
+++ routers/matricula_router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, Form
 from ..db.db import SessionDep
 from sqlmodel import select
 from ..models.matricula import Matricula
+from ..models.estudiante import Estudiante
 from ..utils.enum import EstadoMatricula
 
 router = APIRouter(prefix="/matricula", tags=["Matriculas"])
@@ -13,6 +14,18 @@ async def matricularEstudiante(
     codigo: str = Form(...),
     cedula: str = Form(...)
     ):
+
+    # Convertir el codigo a mayuscula
+    codigo = codigo.upper()
+
+    # Validar que la cedula sea numerica
+    if not cedula.isdigit():
+        raise HTTPException(400, "La cedula debe ser numerica")
+    
+    # Validar que la CC sea valida
+    if not 7 <= len(cedula) <= 10:
+        raise HTTPException(400, "La cedula debe tener entre 7 y 10 numeros")
+
     # Validar si el matricula ya existe
     matriculaDB = session.exec(select(Matricula).where(Matricula.codigo == codigo, Matricula.cedula == cedula)).first()
     # Validar si el estudiante ya esta con estado MATRICULADO en ese curso
@@ -63,6 +76,16 @@ async def listaMatriculas(session: SessionDep):
 # READ - Obtener un estudiante y sus cursos
 @router.get("/estudiante/{cedula}", response_model=list[Matricula])
 async def cursosDeEstudiante(cedula: str, session: SessionDep):
+    # Validar que la cedula sea numerica
+    if not cedula.isdigit():
+        raise HTTPException(400, "La cedula debe ser numerica")
+
+    # Validar si ya existe el estudiante
+    estudianteDB = session.exec(select(Estudiante).where(Estudiante.cedula == cedula)).first()
+    # Si no existe la matricula
+    if not estudianteDB:
+        raise HTTPException(404, "Estudiante no encontrado")
+
     # Validar si el estudiante existe
     estudianteDB = session.exec(select(Matricula).where(Matricula.cedula == cedula, Matricula.matriculado == EstadoMatricula.MATRICULADO)).first()
     if not estudianteDB:
@@ -78,6 +101,9 @@ async def cursosDeEstudiante(cedula: str, session: SessionDep):
 # READ - Obtener un curso y sus estudiantes asociados
 @router.get("/curso/{codigo}", response_model=list[Matricula])
 async def estudiantesEnCurso(codigo: str, session: SessionDep):
+    # Convertir el codigo a mayuscula
+    codigo = codigo.upper()
+
     matriculaDB = session.exec(select(Matricula).where(Matricula.codigo == codigo, Matricula.matriculado == EstadoMatricula.MATRICULADO)).all()
     if not matriculaDB:
         raise HTTPException(404, "No hay estudiantes en este curso")
@@ -94,13 +120,35 @@ async def actualizarMatricula(
     cedula: str = Form(...)
     ):
 
+    # Convertir el codigo a mayuscula
+    codigo = codigo.upper()
+
+    # Validar que la cedula sea numerica
+    if not cedula.isdigit():
+        raise HTTPException(400, "La cedula debe ser numerica")
+    
+    # Validar que la CC sea valida
+    if not 7 <= len(cedula) <= 10:
+        raise HTTPException(400, "La cedula debe tener entre 7 y 10 numeros")
+
     # Verificar que exista la matricula
     matriculaDB = session.get(Matricula, matriculaID)
     if not matriculaDB:
         raise HTTPException(404, "La matricula no existe")
+    
+    # No permitir que se modifique una matricula finalizada
+    if matriculaDB.matriculado == EstadoMatricula.FINALIZADO:
+        raise HTTPException(404, "No puedes modificar esta matricula por que fue finalizada")
 
     # Verificar que no haya otra matricula con los id de estudiante y curso que ingresan
-    existeMatricula = session.exec(select(Matricula).where(Matricula.codigo == codigo, Matricula.cedula == cedula)).first()
+    existeMatricula = session.exec(
+        select(Matricula).where(
+            Matricula.codigo == codigo,
+            Matricula.cedula == cedula,
+            Matricula.matriculado == EstadoMatricula.MATRICULADO
+            )
+        ).first()
+    # Si existe la matricula
     if existeMatricula:
         raise HTTPException(400, "Ya existe esa matricula")
     
@@ -117,9 +165,72 @@ async def actualizarMatricula(
 
 
 
+# PATCH - Finalizacion de un curso por parte de un estudiante
+@router.patch("/{cedula}/finalizar", response_model=Matricula)
+async def finalizarCurso(cedula: str, codigo: str, session: SessionDep):
+    # Convertir el codigo a mayuscula
+    codigo = codigo.upper()
+
+    # Validar que la cedula sea numerica
+    if not cedula.isdigit():
+        raise HTTPException(400, "La cedula debe ser numerica")
+    
+    # Validar que la CC sea valida
+    if not 7 <= len(cedula) <= 10:
+        raise HTTPException(400, "La cedula debe tener entre 7 y 10 numeros")
+
+    # Validar si ya existe el estudiante
+    estudianteDB = session.exec(select(Estudiante).where(Estudiante.cedula == cedula)).first()
+    # Si no existe la matricula
+    if not estudianteDB:
+        raise HTTPException(404, "Estudiante no encontrado")
+
+    # Validar si ya existe una matricula
+    matriculaDB = session.exec(
+        select(Matricula).where(
+            Matricula.codigo == codigo,
+            Matricula.cedula == cedula,
+            Matricula.matriculado == EstadoMatricula.MATRICULADO
+            )
+        ).first()
+    
+    # Si no existe la matricula
+    if not matriculaDB:
+        raise HTTPException(404, "Matricula no encontrada")
+    
+    # Finalizar el curso si esta matriculado
+    if matriculaDB.matriculado == EstadoMatricula.MATRICULADO:
+        matriculaDB.matriculado = EstadoMatricula.FINALIZADO
+    
+    # Insertar la matricula actualizada a la DB
+    session.add(matriculaDB)
+    session.commit() # Guardar los cambios
+    session.refresh(matriculaDB)
+
+    return matriculaDB
+
+
+
 # PATCH - Volver a matricular a un estudiante en un curso
 @router.patch("/{cedula}/rematricular", response_model=Matricula)
 async def rematricularEstudiante(cedula: str, codigo: str, session: SessionDep):
+    # Convertir el codigo a mayuscula
+    codigo = codigo.upper()
+
+    # Validar que la cedula sea numerica
+    if not cedula.isdigit():
+        raise HTTPException(400, "La cedula debe ser numerica")
+    
+    # Validar que la CC sea valida
+    if not 7 <= len(cedula) <= 10:
+        raise HTTPException(400, "La cedula debe tener entre 7 y 10 numeros")
+
+    # Validar si ya existe el estudiante
+    estudianteDB = session.exec(select(Estudiante).where(Estudiante.cedula == cedula)).first()
+    # Si no existe la matricula
+    if not estudianteDB:
+        raise HTTPException(404, "Estudiante no encontrado")
+
     # Validar si ya existe una matricula
     matriculaDB = session.exec(select(Matricula).where(Matricula.codigo == codigo, Matricula.cedula == cedula)).first()
     # Si no existe la matricula
@@ -129,6 +240,10 @@ async def rematricularEstudiante(cedula: str, codigo: str, session: SessionDep):
     # Validar si el estudiante ya habia sido matriculado en ese curso
     if matriculaDB.matriculado == EstadoMatricula.MATRICULADO:
         raise HTTPException(400, "El estudiante ya esta matriculado en ese curso")
+    
+    # Validar si el estudiante ya finalizo el cuso
+    if matriculaDB.matriculado == EstadoMatricula.FINALIZADO:
+        raise HTTPException(400, "El estudiante ya hizo este curso. No puede repetirlo")
     
     # Rematricular al estudiante si estaba desmatriculado
     matriculaDB.matriculado = EstadoMatricula.MATRICULADO
@@ -144,15 +259,34 @@ async def rematricularEstudiante(cedula: str, codigo: str, session: SessionDep):
 # DELETE - Desmatricular a un estudiante de un curso
 @router.delete("/{cedula}/desmatricular", response_model=Matricula)
 async def desmatricularEstudiante(cedula: str, codigo: str, session: SessionDep):
+    # Convertir el codigo a mayuscula
+    codigo = codigo.upper()
+
+    # Validar que la cedula sea numerica
+    if not cedula.isdigit():
+        raise HTTPException(400, "La cedula debe ser numerica")
+    
+    # Validar que la CC sea valida
+    if not 7 <= len(cedula) <= 10:
+        raise HTTPException(400, "La cedula debe tener entre 7 y 10 numeros")
+
+    # Validar si ya existe el estudiante
+    estudianteDB = session.exec(select(Estudiante).where(Estudiante.cedula == cedula)).first()
+    # Si no existe la matricula
+    if not estudianteDB:
+        raise HTTPException(404, "Estudiante no encontrado")
+
     # Validar si ya existe una matricula
-    matriculaDB = session.exec(select(Matricula).where(Matricula.codigo == codigo, Matricula.cedula == cedula)).first()
+    matriculaDB = session.exec(
+        select(Matricula).where(
+            Matricula.codigo == codigo,
+            Matricula.cedula == cedula,
+            Matricula.matriculado == EstadoMatricula.MATRICULADO
+            )
+        ).first()
     # Si no existe la matricula
     if not matriculaDB:
         raise HTTPException(404, "Matricula no encontrada")
-    
-    # Validar si el estudiante ya habia sido desmatriculado de ese curso
-    if matriculaDB.matriculado == EstadoMatricula.DESMATRICULADO:
-        raise HTTPException(400, "El estudiante ya habia sido desmatriculado")
     
     # Desmatricular al estudiante si no se habia hecho antes
     matriculaDB.matriculado = EstadoMatricula.DESMATRICULADO
