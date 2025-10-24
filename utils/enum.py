1e59d5f6123cdf4a7e48f521d3f98572900fe78a
Josue-Ribero <josueribero95@gmail.com>
2025-10-23 22:09:32 -0500

    Cambio en tipo de dato de enumeraciones de creditos y semestres

--- utils/enum.py
+++ utils/enum.py
@@ -5,10 +5,10 @@ from enum import Enum
 
 # Cantidad de creditos de un curso
 class CreditosCurso(Enum):
-    UNO = 1
-    DOS = 2
-    TRES = 3
-    CUATRO = 4
+    UNO = "1"
+    DOS = "2"
+    TRES = "3"
+    CUATRO = "4"
 
 # Horarios cursos
 class JornadaCurso(Enum):
@@ -20,18 +20,18 @@ class JornadaCurso(Enum):
 
 # Semestre que esta cursando el estudiante
 class Semestre(Enum):
-    PRIMERO = 1
-    SEGUNDO = 2
-    TERCERO = 3
-    CUARTO = 4
-    QUINTO = 5
-    SEXTO = 6
-    SEPTIMO = 7
-    OCTAVO = 8
-    NOVENO = 9
-    DECIMO = 10
-    UNDECIMO = 11
-    DUODECIMO = 12
+    PRIMERO = "1"
+    SEGUNDO = "2"
+    TERCERO = "3"
+    CUARTO = "4"
+    QUINTO = "5"
+    SEXTO = "6"
+    SEPTIMO = "7"
+    OCTAVO = "8"
+    NOVENO = "9"
+    DECIMO = "10"
+    UNDECIMO = "11"
+    DUODECIMO = "12"
 
 
 # ENUMERACIONES MATRICULAS
