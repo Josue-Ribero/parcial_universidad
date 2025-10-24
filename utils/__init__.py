fcaf2520b82289c97f0b27b29c2121a580d76e08
Josue-Ribero <josueribero95@gmail.com>
2025-10-23 20:57:01 -0500

    init de las enumeraciones

---
+++ utils/__init__.py
@@ -0,0 +1,13 @@
+from .enum import (
+    CreditosCurso,
+    JornadaCurso,
+    Semestre,
+    EstadoMatricula,
+)
+
+__all__ = [
+    "CreditosCurso",
+    "JornadaCurso",
+    "Semestre",
+    "EstadoMatricula",
+]
